a5ca191653563c01d653f7fde57738166456568d
Nhan <nhan2151@gmail.com>
2019-06-08 17:44:46 +0700

    User Authentication / Login Route

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const gravatar = require('gravatar');
 const  bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const { check,  validationResult } = require('express-validator/check');
 const config = require('config');
+const { check,  validationResult } = require('express-validator/check');
 
 const User = require('../../models/User');
 
