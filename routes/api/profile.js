6a57e636d40585ced9283403be7c5c9fa1db9fe3
Nhan <nhan2151@gmail.com>
2019-06-10 23:05:33 +0700

    Create and Upadate Profile Routes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -64,10 +64,51 @@ router.post(
             linkedin
         } = req.body;
 
-        // console.log(company);
         // Build profile object
         const profileFields = {};
         profileFields.user = req.user.id;
+        if(company) profileFields.company = company;
+        if(website) profileFields.website = website;
+        if(location) profileFields.location = location;
+        if(bio) profileFields.bio = bio;
+        if(status) profileFields.status = status;
+        if(githubusername) profileFields.githubusername = githubusername;
+        if(skills) {
+            profileFields.skills = skills.split(',').map(skill => skill.trim());
+            console.log(profileFields.skills);
+        }
+
+        // Build social object
+        profileFields.social = {}
+        if (youtube) profileFields.social.youtube = youtube;
+        if (twitter) profileFields.social.twitter = twitter;
+        if (facebook) profileFields.social.facebook = facebook;
+        if (linkedin) profileFields.social.linkedin = linkedin;
+        if (instagram) profileFields.social.youtube = instagram;
+
+        try {
+            let profile = await Profile.findOne({ user: req.user.id });
+            if(profile){
+                // Update
+                profile = await Profile.findOneAndUpdate(
+                    { user: req.user.id },
+                    { $set: profileFields },
+                    { new: true },
+                );
+
+                return res.json(profile);
+            }
+
+            // Create
+            profile = new Profile(profileFields);
+
+            await profile.save();
+            res.json(profile);
+
+        } catch (error) {
+            console.error(err.message);
+            res.status(500).send('Server Error')
+        }
     }
 );
 
