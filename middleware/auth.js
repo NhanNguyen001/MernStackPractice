9f977ec8f9f7594e4fa1a572ce2eba2867b6b0da
Nhan <nhan2151@gmail.com>
2019-06-19 01:55:01 +0700

    Get Github Repos For Profile

--- middleware/auth.js
+++ middleware/auth.js
@@ -13,7 +13,6 @@ module.exports = function(req, res, next) {
   // Verify token
   try {
     const decoded = jwt.verify(token, config.get('jwtSecret'));
-    console.log('After decoded user: ', decoded);    
     req.user = decoded.user;
     next();
   } catch (err) {
