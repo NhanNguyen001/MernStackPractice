a9d3d044e04ee9adf965e661e3f7855b1de685d9
Nhan <nhan2151@gmail.com>
2019-06-25 23:34:42 +0700

    Front End Side/Complete User Login

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -39,7 +39,9 @@ router.post('/',
     async (req, res) => {
         const errors = validationResult(req);
         if(!errors.isEmpty()){
-            return res.status(400).json({ errors: errors.array() });
+            return res
+                .status(400)
+                .json({ errors: errors.array() });
         }
 
         const { email, password } = req.body;
