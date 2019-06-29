fc5b64bee1296528ca0b3c6c12867623df3577b3
Nhan <nhan2151@gmail.com>
2019-06-29 18:29:02 +0700

    Complete Dashboard & Profile Management

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -2,7 +2,6 @@ import uuid from 'uuid';
 import { SET_ALERT, REMOVE_ALERT } from './types';
 
 export const setAlert = (msg, alertType, timeout = 5000) => dispatch => {
-  console.log(msg)
   const id = uuid();
   dispatch({
     type: SET_ALERT,
